82a925f428b4a7a0ce2144250e4f6359cd3022ad
David Mendez-Acuna <damenac@gmail.com>
2016-07-28 10:13:46 +0200

    First commit

---
+++ test.challenge.algorithm/src/root/ISort.java
@@ -0,0 +1,12 @@
+package root;
+
+import java.util.List;
+
+public interface ISort {
+
+	/**
+	 * Sorts the list of integers given in the parameter.
+	 * @param numbers
+	 */
+	public void sort(List<Integer> numbers);
+}
